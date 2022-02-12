4dc7c4d629fb96f92ffab91b1af5380ecca8fdf9
husseinAbdElaziz <husseinabdelaziz8@gmail.com>
2022-02-12 23:14:08 +0200

    feat: add ssr && rss feeds server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ssr';
+
+
+  ngOnInit(): void {
+    localStorage.setItem('test', 'test');
+
+    console.log(localStorage.getItem('test'));
+
+  }
+
+
 }
